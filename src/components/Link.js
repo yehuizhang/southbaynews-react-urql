34f88f8eb49f8af4d85b404ff96efb118b317a6f
Yehui <yehuizhang@outlook.com>
2020-02-18 18:13:35 -0800

    Implemented Search function and enhanced list related info

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,56 @@
 import React from "react";
+import gql from "graphql-tag";
+import { useMutation } from "urql";
 
-const Link = ({ link }) => {
+import { getToken } from "../token";
+import { timeDifferenceForDate } from "../dates";
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Link = ({ index, link }) => {
+  const isLoggedIn = !!getToken();
+
+  const [state, executeMutation] = useMutation(VOTE_MUTATION);
+
+  const upvote = React.useCallback(() => {
+    if (!state.fetching) {
+      executeMutation({ linkId: link.id });
+    }
+  }, [state.fetching, executeMutation, link.id]);
+
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {isLoggedIn && (
+          <div className="ml1 gray f11" onClick={upvote}>
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{" "}
+          {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
       </div>
     </div>
   );
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -11,6 +11,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -26,8 +36,8 @@ const LinkList = () => {
 
   return (
     <div>
-      {linksToRender.map(link => (
-        <Link key={link.id} link={link} />
+      {linksToRender.map((link, index) => (
+        <Link key={link.id} link={link} index={index} />
       ))}
     </div>
   );
